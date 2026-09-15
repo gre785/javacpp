4e573c38e42efc911dd3542e259aa497702a0401
gyang <gre785@gmail.com>
2026-09-15 12:59:13 -0400

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.bytedeco.javacv.FFmpegFrameRecorder;
+
+public class DemoApplication {
+
+FFmpegFrameRecorder recorder;
+
+	public static void main(String[] args) {
+		System.out.println("x");
+	}
+
+}
